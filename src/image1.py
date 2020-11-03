09014f9bc690e687468754f1875fc1ffe924a83e
Justin Howe <howejust@gmail.com>
2020-11-04 00:16:31 +0800

    Initial blob detection

--- src/image1.py
+++ src/image1.py
@@ -13,8 +13,6 @@ from cv_bridge import CvBridge, CvBridgeError
 
 class image_converter:
 
-  # Isolate colors within image
-
   # Defines publisher and subscriber
   def __init__(self):
     # initialize the node named image_processing
@@ -25,6 +23,88 @@ class image_converter:
     self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
+  
+    # Isolate the 4 blobs to help obtain 3 joints using color detection
+  def detect_red(self, image):
+    # Isolate red color- threshold slightly differs!!!
+    red_mask = cv2.inRange(image, (0,0,100), (35,35,255))
+    # Kernel convolution created to dilate the red_mask image for safety
+    kernel = np.ones((5,5), np.uint8)
+    red_mask = cv2.dilate(red_mask, kernel, iterations = 1)
+
+    # Obtain moments of binary image
+    M = cv2.moments(red_mask)
+
+    # Calculate pixel coordinates for blob center
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    # Test function to show image window of mask
+    im1 = cv2.imshow('red_mask_window', red_mask)
+    cv2.waitKey(1)
+
+    return np.array([cx,cy])
+
+  def detect_blue(self, image):
+    # Isolate blue color
+    blue_mask = cv2.inRange(image, (100,0,0), (255,75,75))
+    # Kernel convolution created to dilate the blue_mask image
+    kernel = np.ones((5,5), np.uint8)
+    blue_mask = cv2.dilate(blue_mask, kernel, iterations = 3)
+
+    # Obtain moments of binary image
+    M = cv2.moments(blue_mask)
+
+    # Calculate pixel coordinates for blob center
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    # Test function to show image window of mask
+    im1 = cv2.imshow('blue_mask_window', blue_mask)
+    cv2.waitKey(1)
+
+    return np.array([cx,cy])
+  
+  def detect_green(self, image):
+    # Isolate green color- threshold slightly differs!
+    green_mask = cv2.inRange(image, (0,100,0), (25,255,25))
+    # Kernel convolution created to dilate the green_mask image
+    kernel = np.ones((5,5), np.uint8)
+    green_mask = cv2.dilate(green_mask, kernel, iterations = 3)
+
+    # Obtain moments of binary image
+    M = cv2.moments(green_mask)
+
+    # Calculate pixel coordinates for blob center
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    # Test function to show image window of mask
+    im1 = cv2.imshow('green_mask_window', green_mask)
+    cv2.waitKey(1)
+
+    return np.array([cx,cy])
+
+  def detect_yellow(self, image):
+    # Isolate yellow color- threshold slightly differs!
+    yellow_mask = cv2.inRange(image, (0,100,100), (0,255,255))
+
+    # Kernel convolution created to dilate the yellow_mask image
+    kernel = np.ones((5,5), np.uint8)
+    yellow_mask = cv2.dilate(yellow_mask, kernel, iterations = 3)
+
+    # Obtain moments of binary image
+    M = cv2.moments(yellow_mask)
+
+    # Calculate pixel coordinates for blob center
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    # Test function to show image window of mask
+    im1 = cv2.imshow('yellow_mask_window', yellow_mask)
+    cv2.waitKey(1)
+
+    return np.array([cx,cy])
 
 
   # Recieve data from camera 1, process it, and publish
@@ -40,6 +120,15 @@ class image_converter:
 
     im1=cv2.imshow('window1', self.cv_image1)
     cv2.waitKey(1)
+
+    # Test call image detection functions for showing masks respectively
+    self.detect_red(self.cv_image1)
+    self.detect_blue(self.cv_image1)
+    self.detect_green(self.cv_image1)
+    self.detect_yellow(self.cv_image1)
+
+    # TODO: Convert image to HSV format for clearer object detection and then publish it for joints
+
     # Publish the results
     try: 
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
